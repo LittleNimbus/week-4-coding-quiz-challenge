16184c1f3214d97037c3d2f9ef3d4f71db3fc6b7
KnightNimbus <ty.s.spain@gmail.com>
2023-10-27 18:12:48 +0100

    listed js elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var startButton = document.querySelector(".container h1");
+var timerCount = document.getElementById("timer");
+var buttonOne = document.querySelector("#btn0");
+var buttonTwo = document.querySelector("#btn1");
+var buttonThree = document.querySelector("#btn2");  
+var buttonFour = document.querySelector("#btn3");
+var progressBar = document.querySelector ("#progress")
